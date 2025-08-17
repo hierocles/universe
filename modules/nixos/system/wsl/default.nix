a7415caf40d90d5bd32de807e0298572c2a30795
Dylan Henrich <4733259+hierocles@users.noreply.github.com>
2025-08-17 18:15:50 +0000

    wsl = false by default

--- modules/nixos/system/wsl/default.nix
+++ modules/nixos/system/wsl/default.nix
@@ -10,7 +10,7 @@ with lib.${namespace}; let
   cfg = config.${namespace}.system.wsl;
 in {
   options.${namespace}.system.wsl = with types; {
-    enable = mkBoolOpt true "Whether or not to manage WSL configuration.";
+    enable = mkBoolOpt false "Whether or not to manage WSL configuration.";
     default-user = mkOpt str "root" "The default user to use for wsl.";
   };
 
