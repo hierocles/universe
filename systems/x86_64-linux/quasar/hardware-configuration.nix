72bd9e41a80bfdcf25b55478f3f8a573d63cf77f
Dylan Henrich <4733259+hierocles@users.noreply.github.com>
2025-08-17 21:56:40 +0000

    update quasar hardware

--- systems/x86_64-linux/quasar/hardware-configuration.nix
+++ systems/x86_64-linux/quasar/hardware-configuration.nix
@@ -1,5 +1,16 @@
 {
+  config,
+  pkgs,
+  ...
+}: {
   hardware.cpu.intel.updateMicrocode = true;
   boot.kernelParams = ["i915.enable_guc=2"];
-  hardware.intelgpu.vaapiDriver = "intel-media-driver";
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      vaapiIntel
+      intel-media-driver
+      intel-media-sdk
+    ];
+  };
 }
