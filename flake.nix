d11ddbda23d423a3204f6827d0ee6b5e5152ad34
Dylan Henrich <4733259+hierocles@users.noreply.github.com>
2025-08-18 21:55:46 +0000

    made secrets repo a flake

--- flake.nix
+++ flake.nix
@@ -62,7 +62,6 @@
     # Secret repo, contains secrets that don't need to be encrypted
     variables = {
       url = "git+ssh://git@github.com/hierocles/snowfall-secrets?ref=main&shallow=1";
-      flake = false;
     };
   };
 
